ccb987c7061c0e0cfa0b46ca8ac83b9cf0f69d9e
Zach <81403529+ZachPregl@users.noreply.github.com>
2025-02-16 15:03:30 +1000

    Create RomanToInteger.cs

---
+++ Thirteen/RomanToInteger.cs
@@ -0,0 +1,32 @@
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> map = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        int result = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i + 1 < s.Length && map[s[i]] < map[s[i + 1]])
+            {
+                result -= map[s[i]];
+            }
+            else
+            {
+                result += map[s[i]];
+            }
+        }
+
+        return result;
+    }
+}
